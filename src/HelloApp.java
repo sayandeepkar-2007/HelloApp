e9640c2fdc1ffa5c58f2a91660b86e9d4e9f2017
sayandeepkar-2007 <sayandeepkar28@gmail.com>
2026-04-24 10:15:24 +0530

    add UC4

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,12 +1,14 @@
 public class HelloApp {
+
     public static void main(String[] args) {
-        String name = "World";
-        
-        if (args.length > 0) {
-            name = args[0];
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
         }
 
-        System.out.println("Hello, " + name + "!");
-        
     }
 }
