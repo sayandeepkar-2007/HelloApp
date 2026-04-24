690fb7f409183e169733e829438a203f25a87516
sayandeepkar-2007 <sayandeepkar28@gmail.com>
2026-04-24 10:21:20 +0530

    add UC5

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -3,10 +3,10 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
+            for (String name : args) {
+                System.out.println("Hello " + name);
             }
         }
 
