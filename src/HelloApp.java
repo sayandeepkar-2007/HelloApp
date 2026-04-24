3bc5e7369853ed06023e98120ef6e6efc3ab8989
sayandeepkar-2007 <sayandeepkar28@gmail.com>
2026-04-24 10:26:29 +0530

    add UC6

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -5,9 +5,16 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+            String result = "";
+
             for (String name : args) {
-                System.out.println("Hello " + name);
+                result += name + ", ";
             }
+
+            // Remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
         }
 
     }
